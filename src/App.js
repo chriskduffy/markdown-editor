cc5e10a0a8a406fb25ffa6133f901b70aa4f4a6b
ChriskDuffy <73856446+chriskduffy@users.noreply.github.com>
2021-01-08 22:57:04 +0000

    initialising

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import defaultText from './defaultText';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const initText = `Marked - Markdown Parser
+========================
+
+[Marked] lets you convert [Markdown] into HTML.  Markdown is a simple text format whose goal is to be very easy to read and write, even when not converted to HTML.  This demo page will let you type anything you like and see how it gets converted.  Live.  No more waiting around.
+
+How To Use The Demo
+-------------------
+
+1. Type in stuff on the left.
+2. See the live updates on the right.
+
+That's it.  Pretty simple.  There's also a drop-down option in the upper right to switch between various views:
+
+- **Preview:**  A live display of the generated HTML as it would render in a browser.
+- **HTML Source:**  The generated HTML before your browser makes it pretty.
+- **Lexer Data:**  What [marked] uses internally, in case you like gory stuff like this.
+- **Quick Reference:**  A brief run-down of how to format things using markdown.
+
+[Marked]: https://github.com/markedjs/marked/
+[Markdown]: http://daringfireball.net/projects/markdown/
+`
+
+const marked = require ('marked');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const window = new JSDOM('').window;
+const DOMPurify = createDomPurify(window);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (event) => {
+      this.setState({ input: event.target.value });
+    }
+
+    processText = () => {
+      let dirtyHTML = marked(this.state.input, {breaks: true, });
+      let cleanHTML = DOMPurify.sanitize(dirtyHTML); // Sanitize HTML using dompurify
+      return cleanHTML;
+    }
+
+    componentDidMount() {
+      this.setState({ input: initText })
+    }
+
+    render () {
+      return (
+        <>
+        <div id="header">
+          <h1 id="title">Markdown Previewer</h1>
+        </div>
+        <div id="container">
+          <div id="textEditor">
+          <textarea id="editor" placeholder="Enter markdown text here" value={this.state.input} onChange={this.handleChange}></textarea>
+          </div>
+          <div id="previewArea">
+            <div id="preview">
+            <div dangerouslySetInnerHTML={{__html: this.processText()}} />
+            </div>
+            <div id="raw-html-block">
+              <h3>Raw HTML</h3>
+              <p id="raw-html">{this.processText()}</p>
+            </div>
+          </div>
+        </div>
+        </>
+      );
+    }
+  };
 
 export default App;
